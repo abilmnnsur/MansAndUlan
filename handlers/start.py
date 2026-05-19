8a85ecefb08ec8cfc78cb2d24246e509d73dca13
ulanahmet7-svg <ulanahmet7@gmail.com>
2026-05-19 21:11:25 +0500

    feat: complete Help/About handlers and finalize bot logic

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,14 @@
 from aiogram import Router
-from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.filters import Command
+
+from keyboards.menu import get_main_menu   # ← ДОБАВИТЬ
 
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-    print("Команда /start получена!")
-    await message.answer("Привет! Я бот для генерации QR-кодов. Выбери действие.")
+    await message.answer(
+        "Привет! Я бот для генерации QR-кодов. Выбери действие.",
+        reply_markup=get_main_menu()
+    )
