d9440433b8febdbd3a6fd7d125e72e0d773d85ef
ulanahmet7-svg <ulanahmet7@gmail.com>
2026-05-19 20:50:57 +0500

    feat: add __init__.py files for project structure

--- handlers/start.py
+++ handlers/start.py
@@ -1,27 +1,10 @@
-from aiogram import Router, F
+from aiogram import Router
+from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
-from keyboards.menu import get_main_menu
 
 router = Router()
 
-
-@router.message(CommandStart())
+@router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer(
-        "Привет! Я бот для генерации QR-кодов. Используй меню ниже:",
-        reply_markup=get_main_menu(),
-    )
-
-
-@router.message(F.text == "Help")
-@router.message(Command("help"))
-async def cmd_help(message: Message):
-    await message.answer(
-        "Нажми 'Generate QR', выбери тип данных, отправь текст и получи картинку!"
-    )
-
-
-@router.message(F.text == "About")
-async def cmd_about(message: Message):
-    await message.answer("Курсовой проект.\nСтек: aiogram 3.x, requests, OOP, JSON.")
+    print("Команда /start получена!")
+    await message.answer("Привет! Я бот для генерации QR-кодов. Выбери действие.")
